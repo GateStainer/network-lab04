17ae8d4629b1920675a471148c124f29c080b02f
PUXI <puxinju@gmail.com>
2018-04-12 20:32:59 +0800

    Complete login and logout.

--- tcp_client.c
+++ tcp_client.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <pthread.h>
+
 
 #include "user.h"
 
@@ -12,11 +14,76 @@
 #define MAXLINE 4096
 #define SERV_PORT 8888
 
+int state = -1;
+
+
+void printWelcome()
+{
+	system("clear");
+	printf("Welcome to Battle Game online.\n");
+	printf("(#) exit\t(h) help\n");
+	printf("Please enter your user name:");
+}
+
+void printHelp()
+{
+	system("clear");
+	printf("TODO: print help message\n");
+	printf("(r) back\t(#) exit\n");
+}
+
+void* monitor_handler(void* socket_desc)
+{
+	char recvline[MAXLINE];
+	int sock = *(int*)socket_desc;
+	int read_size;
+	while((read_size = recv(sock, recvline, MAXLINE, 0)) > 0)
+	{
+
+		TcpData* data = (TcpData*)recvline;
+		switch(state){
+			case 0:
+				switch(data->category) {
+					case 0x01:
+						// 0x01 stands for login
+						if(data->info == 0x01)
+						{
+							state = 3;
+							printf("%s\n", "Successful login");
+							printf("%s\n", "(#) exit");
+						}
+						else if(data->info == 0x02)
+						{
+							printf("%s\n", "Fail login. There exists user with same name\n");
+						}
+						else
+						{
+							printf("%s\n", "Unknown login response");
+						}
+						break;
+
+					default:
+						;
+				}
+				break;
+			case 3:
+				;
+				break;
+			default:
+				;
+		}
+		memset(recvline, 0, MAXLINE);
+	}
+	return 0;
+
+}
+
+
 int main()
 {
 	int sockfd;
 	struct sockaddr_in servaddr;
-	char sendline[MAXLINE], recvline[MAXLINE];
+	char sendline[MAXLINE];
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&servaddr, 0, sizeof(servaddr));
@@ -25,19 +92,73 @@ int main()
 	servaddr.sin_port = htons(8888);
 
 	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	state = 0;
+	pthread_t thread_id;
+	if(pthread_create(&thread_id, NULL, monitor_handler, (void*) &sockfd) > 0)
+	{
+		perror("Could not create thread");
+		exit(-1);
+	}
+
+	printWelcome();
 	while(fgets(sendline, MAXLINE, stdin) != NULL)
 	{
-		send(sockfd, sendline, strlen(sendline), 0);
-		if(recv(sockfd, recvline, MAXLINE, 0) == 0)
-		{
-			perror("The server terminated");
-			exit(4);
-		}
-		printf("%s", "String received from server: ");
-		fputs(recvline, stdout);
+		int i = strlen(sendline);
+		if(sendline[i-1]=='\n')
+			sendline[i-1] = '\0';
+		if(strlen(sendline) == 0)
+			continue;
+		switch(state) {
+			case 0:
+				if(strcmp(sendline, "#")==0)
+				{
+					close(sockfd);
+					exit(0);
+				}
+				else if(strcmp(sendline, "h")==0)
+				{
+					printHelp();
+					state = 2;
+				}
+				else
+				{
+					//send login request
+					TcpData data;
+					data.category = 0x01;
+					strcpy(data.user_name, sendline);
+					send(sockfd, (char*)&data, sizeof(TcpData), 0);
+				}
+				break;
+			case 2:
+				if(strcmp(sendline, "#")==0)
+				{
+					close(sockfd);
+					exit(0);
+				}
+				else if(strcmp(sendline, "r")==0)
+				{
+					printWelcome();
+					state = 0;
+				}
+				break;
+			case 3:
+				if(strcmp(sendline, "#")==0)
+				{
+					close(sockfd);
+					exit(0);
+				}
 
+				break;
+			default:
+				;
+		}
+		/*
+		TcpData data;
+		data.category = 0x01;
+		strcpy(data.user_name, sendline);
+		send(sockfd, (char*)&data, sizeof(TcpData), 0);
+		*/
 		memset(sendline, 0, MAXLINE);
-		memset(recvline, 0, MAXLINE);
 
 	}
 	exit(0);
--- tcp_server.c
+++ tcp_server.c
@@ -2,12 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <iostream>
+#include <iterator>
+#include <set>
 
 #include "user.h"
-extern map<string, user_info> user_data;
+
+extern unordered_map<string, struct user_info> user_table;
+
 
 #define MAXLINE 4096
 #define SERV_PORT 8888
@@ -65,17 +71,54 @@ void* connection_handler(void* socket_desc)
 	char *message;
 	char client_message[MAXLINE];
 
-	//message = "Greetings! I am your connection handler\n";
-	//write(sock, message, strlen(message));
 
 	while((read_size = recv(sock, client_message, MAXLINE, 0)) > 0)
 	{
-		client_message[read_size] = '\0';
-		write(sock, client_message, strlen(client_message));
+		TcpData* data = (TcpData*)client_message;
+		switch(data->category) {
+			case 0x01:
+				/* 0x01 stands for login
+				first check whether there is an online user with same name
+				if not, add the user into user_table or change user state to 1, else return bad request*/
+				if(user_table.find(data->user_name)==user_table.end() || user_table[data->user_name].state!=1)
+				{
+					UserInfo userinfo;
+					userinfo.state = 1;
+					userinfo.current_life = 10;
+					userinfo.sock = sock;
+					user_table.insert({data->user_name, userinfo});
+					data->info = 0x01; //success
+					printf("%s\n", "Success login");
+					printf("Total user number: %d\n", user_table.size());
+					write(sock, data, sizeof(TcpData));
+					
+				}
+				else
+				{
+					data->info = 0x02; //fail to login
+					printf("%s\n", "Fail login");
+					write(sock, data, sizeof(TcpData));
+				}
+				break;
+
+			default:
+				;
+		}
 		memset(client_message, 0, MAXLINE);
 	}
 	if(read_size == 0)
 	{
+		unordered_map<string, UserInfo>::iterator it = user_table.begin();
+		while(it != user_table.end())
+		{
+			if(it->second.sock == sock)
+			{
+				it->second.state = 0; // offline
+				printf("User %s offline\n", &it->first[0]);
+				break;
+			}
+			it++;
+		}
 		puts("Client disconnected");
 		fflush(stdout);
 	}
@@ -83,10 +126,16 @@ void* connection_handler(void* socket_desc)
 	{
 		perror("Receive failed");
 	}
+	close(sock);
 	return 0;
 
 }
 
+
+void broadcast()
+{
+	
+}
 
 
 
